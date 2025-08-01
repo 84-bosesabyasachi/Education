4cffc38e47d99082908cc5005ea1df6bf1b0cc30
84-bosesabyasachi <sabyasachibose1602@gmail.com>
2025-08-01 11:00:35 +0530

    update

---
+++ Day4.java
@@ -0,0 +1,10 @@
+public class Day4 {
+    public static void main(String[] args){
+        int arr[]={1,3,5,4,5,4,3};
+        int ans=0;
+        for(int i: arr){
+            ans=ans^i;
+        }
+        System.out.println(ans);
+    }
+}
