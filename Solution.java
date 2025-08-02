70022df4c6cf0430ab64d5170163e5abd76df427
84-bosesabyasachi <sabyasachibose1602@gmail.com>
2025-08-02 15:06:05 +0530

    update

---
+++ Solution.java
@@ -0,0 +1,22 @@
+class CoinChange {
+    static int minCoins(int[] a, int n) {
+        if(n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i=0; i<a.length; i++) {
+            if(n - a[i] >= 0) {
+                int subAns = minCoins(a, n - a[i]);
+                if(subAns != Integer.MAX_VALUE && subAns + 1 < ans) {
+                    ans = subAns + 1;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int a[] = {7, 5, 1};
+        int n = 18;
+        System.out.println(minCoins(a, n));
+    }
+}
