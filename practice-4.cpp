fad188a9624f3d89ed9ce27a3a811c76491febcd
84-bosesabyasachi <sabyasachibose1602@gmail.com>
2025-08-06 21:01:34 +0530

    update

--- practice-4.cpp
+++ practice-4.cpp
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include <iostream>
 using namespace std;
+
 struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
- 
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
@@ -25,7 +26,40 @@ public:
             prev = cur;
             cur = npn;
         }
-
         return dummy.next;        
     }
 };
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    ListNode* swapped = sol.swapPairs(head);
+
+    cout << "After swapping: ";
+    printList(swapped);
+
+    
+    while (swapped) {
+        ListNode* temp = swapped;
+        swapped = swapped->next;
+        delete temp;
+    }
+
+    return 0;
+}
