afaea4cb9a6c73361fac918ad98527b952b1b09b
84-bosesabyasachi <sabyasachibose1602@gmail.com>
2025-08-12 23:32:51 +0530

    python update

---
+++ NEXT-Permutation.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        nums[i + 1:] = reversed(nums[i + 1:])
+if __name__ == "__main__":
+    nums = [1, 2, 3]
+    Solution().nextPermutation(nums)
+    print(nums)  # Output: [1, 3, 2]
